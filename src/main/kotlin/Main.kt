5fae25ce5e2a72852026930d6d162a85d19c3643
MegaHamer <135736897+MegaHamer@users.noreply.github.com>
2023-09-29 08:14:13 +0300

    2 задание

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 import kotlin.random.Random
 
 fun main() {
-    oneTask()
+    //oneTask()
+    twoTask()
     //threeTask()
 }
 
@@ -9,8 +10,39 @@ fun threeTask(){
     val alf = mapOf('A' to 1,'B' to 2)
 }
 
+fun twoTask(){
+    print("Введите матрицу 5 на 5: ")
+    val col = 5
+    val row = 5
+    val mas = Array(col,{
+        Array(row,{ readln().toInt() })
+    })
+    var boo = true
+    for(i in 0 until col){
+        for (j in 0 until row){
+            if ((mas[i][j]!=mas[j][i]) and (i!=j)){
+                boo=false
+            }
+        }
+    }
+    println("Матрица")
+    for (i in 0 until col){
+        for (j in 0 until row){
+            print("${mas[i][j]} ")
+        }
+        println()
+    }
+    if (boo){
+        println("является симметричным относительно главной диагонали")
+    }else{
+        println("не является симметричным относительно главной диагонали")
+    }
+}
+
 fun oneTask(){
+    print("Введите количесвто строк: ")
     val col = readln().toInt()
+    print("Введите количесвто столбцов: ")
     val row = readln().toInt()
     //Создание 2 мерного массива
     val mas = Array (col,{Array<Int>(row,{
